24629957768cfdb80023b0c21a6f237c0887c521
Mehmet Tarhan <memtarhan@gmail.com>
2020-09-02 00:16:23 +0300

    Created project

---
+++ Beeline/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beeline
+//
+//  Created by Mehmet Tarhan on 2.09.2020.
+//  Copyright © 2020 Mehmet Tarhan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
